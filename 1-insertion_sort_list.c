b621d1ff3d3972a508f47a529874f6d44704036c
jeniffervp <737@holbertonschool.com>
2019-06-14 17:57:33 +0000

    [Feature] prueba

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,6 +9,36 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	holi
+	listint_t *tmp;
+	listint_t *temp2;
+	listint_t *node;
+	listint_t *head;
+	listint_t *insertion;
 
+	head = *list;
+	node = *list;
+	node = node->next;
+	while (node)
+	{
+		insertion = head;
+		while (insertion != node)
+		{
+			temp2 = node;
+			if (insertion->n > node->n)
+			{
+				tmp = node->next;
+				insertion->next = tmp;
+				node->prev = insertion->prev;
+				node->next = insertion;
+				insertion->prev = node;
+				insertion = node;
+			}
+			else
+			{
+				print_list(*list);
+				insertion = insertion->next;
+			}
+		}
+		node = node->next;
+	}
 }
